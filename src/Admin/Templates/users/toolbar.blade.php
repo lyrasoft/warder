af08f5c63ffc2ebe99f7ccac35669ea025217703
Simon Asika <asika32764@gmail.com>
2016-03-08 23:14:12 +0800

    Use btn-sm

--- src/Admin/Templates/users/toolbar.blade.php
+++ src/Admin/Templates/users/toolbar.blade.php
@@ -1,21 +1,21 @@
 {{-- Part of phoenix project. --}}
 
-<a type="button" class="waves-effect waves-light btn btn-success" href="{{ $router->html('user', array('new' => true)) }}">
+<a type="button" class="waves-effect waves-light btn btn-success btn-sm" href="{{ $router->html('user', array('new' => true)) }}">
     <span class="glyphicon glyphicon-plus fa fa-plus"></span>
     @translate('phoenix.toolbar.new')
 </a>
 
-{{--<button type="button" class="waves-effect btn btn-default" onclick="Phoenix.Grid.hasChecked();Phoenix.post();">--}}
+{{--<button type="button" class="waves-effect btn btn-default btn-sm" onclick="Phoenix.Grid.hasChecked();Phoenix.post();">--}}
     {{--<span class="glyphicon glyphicon-duplicate fa fa-copy text-info"></span>--}}
     {{--@translate('phoenix.toolbar.duplicate')--}}
 {{--</button>--}}
 
-<button type="button" class="waves-effect btn btn-default" onclick="Phoenix.Grid.hasChecked();Phoenix.Grid.batch('unblock');">
+<button type="button" class="waves-effect btn btn-default btn-sm" onclick="Phoenix.Grid.hasChecked();Phoenix.Grid.batch('unblock');">
     <span class="glyphicon glyphicon-ok fa fa-check text-success"></span>
     @translate($warderPrefix . 'toolbar.enable')
 </button>
 
-<button type="button" class="waves-effect btn btn-default" onclick="Phoenix.Grid.hasChecked();Phoenix.Grid.batch('block');">
+<button type="button" class="waves-effect btn btn-default btn-sm" onclick="Phoenix.Grid.hasChecked();Phoenix.Grid.batch('block');">
     <span class="glyphicon glyphicon-remove fa fa-remove text-danger"></span>
     @translate($warderPrefix . 'toolbar.disable')
 </button>
@@ -25,7 +25,7 @@
     {{--@translate('phoenix.toolbar.batch')--}}
 {{--</button>--}}
 
-<button type="button" class="waves-effect btn btn-default" onclick="Phoenix.Grid.hasChecked().deleteList();">
+<button type="button" class="waves-effect btn btn-default btn-sm" onclick="Phoenix.Grid.hasChecked().deleteList();">
     <span class="glyphicon glyphicon-trash fa fa-trash"></span>
     @translate('phoenix.toolbar.delete')
 </button>
