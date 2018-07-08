cb3e8a27d9a3f1879f3f74578c006d8a2177c732
Simon Asika <asika32764@gmail.com>
2018-07-08 16:08:36 +0800

    Edit page use sm buttons #79

--- src/Admin/Templates/user/toolbar.blade.php
+++ src/Admin/Templates/user/toolbar.blade.php
@@ -1,11 +1,11 @@
 {{-- Part of phoenix project. --}}
 
-<button type="button" class="btn btn-success" onclick="Phoenix.post();">
+<button type="button" class="btn btn-success btn-sm" onclick="Phoenix.post();">
     <span class="glyphicon glyphicon-floppy-disk fa fa-save"></span>
     @translate('phoenix.toolbar.save')
 </button>
 
-<button type="button" class="btn btn-primary" onclick="Phoenix.post(null, {task: 'save2close'});">
+<button type="button" class="btn btn-primary btn-sm" onclick="Phoenix.post(null, {task: 'save2close'});">
     <span class="glyphicon glyphicon-ok fa fa-check"></span>
     @translate('phoenix.toolbar.save2close')
 </button>
@@ -20,7 +20,7 @@
 {{--@translate('phoenix.toolbar.save2new')--}}
 {{--</button>--}}
 
-<a role="button" class="btn btn-default btn-outline-secondary" href="{{ $router->route('users') }}">
+<a role="button" class="btn btn-default btn-outline-secondary btn-sm" href="{{ $router->route('users') }}">
     <span class="glyphicon glyphicon-remove fa fa-remove fa-times"></span>
     @translate('phoenix.toolbar.cancel')
 </a>
