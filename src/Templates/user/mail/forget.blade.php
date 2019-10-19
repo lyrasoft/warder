690137f6bed2ae069dd665bf98ba96a4d4fddbb3
Simon Asika <asika32764@gmail.com>
2019-10-19 18:40:14 +0800

    Mail layout

--- src/Templates/user/mail/forget.blade.php
+++ src/Templates/user/mail/forget.blade.php
@@ -1,17 +1,41 @@
-{{-- Part of eng4tw project. --}}
+<?php
 
-<p>
-    Hi {{ $user->name }}
-</p>
+/**
+ * Global variables
+ * --------------------------------------------------------------
+ * @var $app           \Windwalker\Web\Application                 Global Application
+ * @var $package       \Windwalker\Core\Package\AbstractPackage    Package object.
+ * @var $view          \Windwalker\Data\Data                       Some information of this view.
+ * @var $uri           \Windwalker\Uri\UriData                     Uri information, example: $uri->path
+ * @var $datetime      \DateTime                                   PHP DateTime object of current time.
+ * @var $helper        \Windwalker\Core\View\Helper\Set\HelperSet  The Windwalker HelperSet object.
+ * @var $router        \Windwalker\Core\Router\PackageRouter       Router object.
+ * @var $asset         \Windwalker\Core\Asset\AssetManager         The Asset manager.
+ *
+ * @var $message       \Windwalker\Core\Mailer\MailMessage
+ * @var $user          \Lyrasoft\Warder\Data\UserData
+ * @var $link          string
+ * @var $token         string
+ */
 
-<p>
-    You send a password reset require at this site.
-</p>
+?>
 
-<p>
-    Your token is: <code>{{ $token }}</code>
-</p>
+@extends('mail.mail-layout')
 
-<p>
-    Please go to: <a href="{{ $link }}">{{ $link }}</a> to reset your password.
-</p>
+@section('content')
+    <p>
+        Hi {{ $user->name }}
+    </p>
+
+    <p>
+        You send a password reset require at this site.
+    </p>
+
+    <p>
+        Your token is: <code>{{ $token }}</code>
+    </p>
+
+    <p>
+        Please go to: <a href="{{ $link }}">{{ $link }}</a> to reset your password.
+    </p>
+@stop
--- src/Templates/user/mail/registration.blade.php
+++ src/Templates/user/mail/registration.blade.php
@@ -1,18 +1,51 @@
-{{-- Part of phoenix project. --}}
+<?php
 
+/**
+ * Global variables
+ * --------------------------------------------------------------
+ * @var $app           \Windwalker\Web\Application                 Global Application
+ * @var $package       \Windwalker\Core\Package\AbstractPackage    Package object.
+ * @var $view          \Windwalker\Data\Data                       Some information of this view.
+ * @var $uri           \Windwalker\Uri\UriData                     Uri information, example: $uri->path
+ * @var $datetime      \DateTime                                   PHP DateTime object of current time.
+ * @var $helper        \Windwalker\Core\View\Helper\Set\HelperSet  The Windwalker HelperSet object.
+ * @var $router        \Windwalker\Core\Router\PackageRouter       Router object.
+ * @var $asset         \Windwalker\Core\Asset\AssetManager         The Asset manager.
+ *
+ * @var $message       \Windwalker\Core\Mailer\MailMessage
+ * @var $user          \Lyrasoft\Warder\Data\UserData
+ * @var $link          string
+ */
 
-<p>
-    Dear {{ $user->name }}
-</p>
+?>
 
-<p>
-    You register a new user account at {{ \Phoenix\Html\HtmlHeader::getSiteName() }}.
-</p>
+@extends('mail.mail-layout')
 
-<p>
-    Please verify your email and go to this URL: <a href="{{ $link }}">{{ $link }}</a>
-</p>
+@section('content')
+    <p>
+        Hi {{ $user->name }}
+    </p>
 
-<p>
-    Thank you.
-</p>
+    <p>
+        You register a new user account at {{ \Phoenix\Html\HtmlHeader::getSiteName() }}.
+        Please verify your email.
+    </p>
+
+    <p>
+        Go to this URL: <a href="{{ $link }}">{{ $link }}</a>
+    </p>
+
+    <p>
+        Or click this button:
+    </p>
+
+    <p>
+        <a class="btn btn-primary" href="{{ $link }}">
+            Verify my email
+        </a>
+    </p>
+
+    <p>
+        Thank you.
+    </p>
+@stop
